2ceeed3629adc18870da1e4693844c5621b4ff25
ElTalGunshy <dp703064@gmail.com>
2026-09-20 22:01:54 -0600

    TRATAMIENTO DE LOS DATOS
    Se revisaron, se limpiaron, se analizaron los datos y además se crearon variables nuevas

--- SRC/Analysis.py
+++ SRC/Analysis.py
@@ -25,3 +25,25 @@ print(df.isnull().sum())
 print("Duplicados:", df.duplicated().sum())
 print(df.describe())
 #----------------------------------------------
+print(df[["Age", "Cabin", "Embarked"]].isnull().sum())
+df["Age"] = df["Age"].fillna(df["Age"].median())
+df["Embarked"] = df["Embarked"].fillna(df["Embarked"].mode()[0])
+df["HasCabin"] = df["Cabin"].notnull()
+df["FamilySize"] = df["SibSp"] + df["Parch"] + 1
+df["Alone"] = df["FamilySize"] == 1
+def categoria_edad(edad):
+    if edad < 13:
+        return "Niño"
+    elif edad < 18:
+        return "Joven"
+    elif edad < 60:
+        return "Adulto"
+    else:
+        return "Adulto mayor"
+df["AgeGroup"] = df["Age"].apply(categoria_edad)
+
+supervivencia = df["Survived"].mean() * 100
+print("Porcentaje de supervivencia:", supervivencia)
+print(df.groupby("Sex")["Survived"].mean() * 100)
+print(df.groupby("Pclass")["Survived"].mean() * 100)
+print(df.groupby("Alone")["Survived"].mean() * 100)

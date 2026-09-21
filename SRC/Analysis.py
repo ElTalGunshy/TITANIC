1238513b6118d26be072598d9a3517399eed42f3
ElTalGunshy <dp703064@gmail.com>
2026-09-20 21:27:26 -0600

    Exploración de lo Datos
    Se hizo una exploración de algunos datos con los que contamos como por ejemplo la cantidad de pasajeros, el tipo de datos de las columnas, registros duplicados, entre otros.

--- SRC/Analysis.py
+++ SRC/Analysis.py
@@ -1,1 +1,27 @@
-pip list
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv("data/train.csv")
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv("data/train.csv")
+print(df)
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv("data/train.csv")
+
+print(df)
+print("Número de pasajeros:", len(df))
+print("Número de columnas:", len(df.columns))
+print("Columnas:")
+print(df.columns)
+print(df.dtypes)
+print(df.isnull().sum())
+print("Duplicados:", df.duplicated().sum())
+print(df.describe())
+#----------------------------------------------
